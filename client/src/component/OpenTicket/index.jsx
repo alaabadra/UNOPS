c2e27813a6ec6ee3ed55082c6ecb4bfc12aa4fbe
alaabadra <alaabadra4@gmail.com>
2019-04-19 20:49:53 +0300

    create routes

---
+++ client/src/component/OpenTicket/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class OpenTicket extends Component {
+    render(){
+        return(
+            <div>
+                OpenTicket
+            </div>
+        )
+    }
+}
