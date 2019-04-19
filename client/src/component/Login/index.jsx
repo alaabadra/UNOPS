c732ece76d8493790ee6c10d68ba91598c74f1f8
alaabadra <alaabadra4@gmail.com>
2019-04-19 22:41:52 +0300

    make page login in front end

--- client/src/component/Login/index.jsx
+++ client/src/component/Login/index.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './style.css';
+import Logo from '../Logo'
 
 export default class Login extends Component {
   render(){
       return(
-          <div>
-              Login
-          </div>
+         <section className="login">
+             <main className="login_main">
+                <div className="header_logo">
+                    <Logo className="login_logo" />
+                    <span className="UN">UN</span>
+                    <span className="OPS">OPS</span>
+                </div>
+                <form action="" className="login_form">
+                <label htmlFor="email" className="login_label">
+                    <span className="text_labelEmail">
+                        Email
+                        <span className="required">*</span>
+                    </span>
+                    <input type="email" id="email" className="login_inputEmail"/>
+                </label>
+                <label htmlFor="password" className="login_label">
+                    <span className="text_labelPassword">
+                        password
+                        <span className="required">*</span>
+                    </span>
+                    <input type="password" id="password" className="login_inputPass"/>
+                </label>
+                    <input type="submit"  id="submit" className="send"/>
+                </form>
+             </main>
+         </section>
       )
   }
 }
